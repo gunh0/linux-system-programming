c808ee3ea57b021209a79de261ef2156f88c159d
Gunho Park <devgun@kakao.com>
2019-07-25 11:05:10 +0900

    upload open

---
+++ open/open.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[])
+{
+	int fd;
+
+	if(argc > 1)
+	{
+		fd = open(argv[1],O_RDONLY);
+		if(fd == -1)
+		{
+			printf("Invalid file name : %s\n",argv[1]);
+		}
+		else
+	{
+		printf("Success to open file : %s\n",argv[1]);
+		close(fd);
+	}
+	}
+
+	else
+	{
+		printf("Usage : %s <filename>\n", argv[0]);
+	}
+
+	return 0;
+}
