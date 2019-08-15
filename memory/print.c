a2a221762333798f73f223530e9e120371a5738c
Gunho Park <devgun@kakao.com>
2019-08-15 23:56:18 +0900

    commit

---
+++ memory/print.c
@@ -0,0 +1,13 @@
+#include <unisted.h>
+#include <stdio.h>
+
+extern etext, edata, end;
+int main(void)
+{
+    char *break_point;
+    printf("exext value : %x\n", &etext);
+    printf("edata value : %x\n", &edata);
+    printf("end value : %x\n", &end);
+    break_point = sbrk(0);
+    printf("current break value -> %x\n", break_point);
+}
