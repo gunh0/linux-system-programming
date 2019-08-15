e17cf8f5f2f3012fe2f14e36836532cafc6f162c
Gunho Park <devgun@kakao.com>
2019-08-16 00:01:13 +0900

    commit

--- memory/print.c
+++ memory/print.c
@@ -11,3 +11,14 @@ int main(void)
     break_point = sbrk(0);
     printf("current break value -> %x\n", break_point);
 }
+
+/*
+int *sbrk(ptrdiff_t increment);
+
+입력 값
+    - increment : 현재의 break값에 더해지는 값
+
+반환 값
+    - 정상 : 이전의 break 값
+    - 에러 : -1
+ */
