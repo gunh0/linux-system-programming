e09b1538556ed9b90bba911de0b72b226e148932
Gunho Park <devgun@kakao.com>
2019-09-14 18:51:47 +0900

    commit

---
+++ alloc/malloc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct record{
+    int no;
+    char name[20];
+};
+
+int main(void)
+{
+    struct record *ptr;
+    ptr = (struct record*)malloc(sizeof(struct record));
+    if(ptr)
+    {
+        ptr->no=1;
+        strcpy(ptr->name, "RTDCS");
+
+        printf("record id %d\n",ptr->no);
+        printf("record name %s\n",ptr->name);
+    }
+    free(ptr);
+    return 0;
+}
