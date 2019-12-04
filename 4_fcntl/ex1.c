fa9eda0bbd1e8a84fec8d5a4b7641912128619bb
Gunho Park <devgun@kakao.com>
2019-12-04 16:28:54 +0900

    commit

---
+++ 4_fcntl/ex1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MESSAGE "message to stdout \n"
+
+int main(void)
+{
+    int newfd;
+    
+    if((newfd=fcntl(STDERR_FILENO,F_DUPFD,3))==-1){
+        perror("fcntl");
+        exit(1);
+    }
+
+    close(STDOUT_FILENO);
+    write(3,MESSAGE,strlen(MESSAGE));   // 실제 모니터로 출력됨
+    return 0;
+}
