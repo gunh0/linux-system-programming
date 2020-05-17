fc2ce665a07076b3660aa4ef4fb01b7edb6fc83d
Gunho Park <devgun@kakao.com>
2020-05-17 20:20:52 +0900

    commit

---
+++ Linux_System_Programming/1_fork_example/forkEX.c
@@ -0,0 +1,29 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int global_int = 5;
+int main()
+{
+    pid_t pid;
+
+    if ((pid = fork()) == 0)    // child
+    {
+        global_int = 6;
+        printf("child's data = %d\n", global_int);
+        exit(0);
+    }
+    else    // parent
+    {
+        global_int = 7;
+        printf("parent's data = %d\n", global_int);
+        wait(NULL);
+    }
+
+    // parent : after join
+
+    printf("parent's data = %d", global_int);
+    return 0;
+}
---
+++ Linux_System_Programming/README.md
@@ -0,0 +1,3 @@
+### The creations and extinction of a process
+
+- Process creation and collaboration by Fork : fork_example
